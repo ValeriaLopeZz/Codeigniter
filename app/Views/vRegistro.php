16d2727a3d2724da5cc075c6e03f41b9db85f1d2
ValeriaLopeZz <marlencitaflores178@gmail.com>
2022-02-23 09:19:50 -0600

    Version_ediatable
    Poder editar archivos de codeigniter en GitHub ya despues subirlos editados y poder trabajarlos en equipo

---
+++ app/Views/vRegistro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro</title>
+</head>
+<body>
+	<div class="container">
+		<center>
+		<h1>Registro de Usuario Nuevo</h1>
+		<form method="POST" action="../Home/insertarForm">
+			<label for="name">Nombre</label>
+			<input type="name" id="name" name="nombre" placeholder="nombre"><br><br>
+			<label for="primer">Primer Apellido</label>
+			<input type="primer" id="primer" name="primer apellido" placeholder="Primer Apellido"><br><br>
+			<label for="segundo">Segundo Apellido</label>
+			<input type="segundo" id="segundo" name="segundo apellido" placeholder="Segundo Apellido"><br><br>
+			<label for="email">Email</label>
+			<input type="email" id="email" name="Email" placeholder="email@example.com"><br><br>
+			<label for="repetir email">Repetir Email</label>
+			<input type="repetir" id="repetir" name="Repetir Email" placeholder="Repetir Email"><br><br>
+			<label for="password">Contraseña</label>
+			<input type="password" id="password" name="Contraseña" placeholder="Contraseña"><br><br>
+			<label for="password">Repetir Contraseña</label>
+			<input type="password" id="password name="Repetir Contraseña" placeholder="Repetir Contraseña"><br><br>
+			<button type="submit">Registrarse</button>
+		</form>
+	</div>
+</body>
+</center>
+<footer>
+	<div class="text-center text-dark p-3" style="background-color: rgba(0,0,0,0,2);">
+		<i class="bi bi-badge-cc"></i> 2022:
+		<a  class="text-dark "href="#">Valeria Marlen Flores Lopez</a>
+	</div>
+</footer>
+</html>
+
+</body>
+</html>
